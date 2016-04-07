9cdf84a58c90a6f0dd16c58b082da8a8ed38267c
lukeliao <luke_liao@126.com>
2016-04-07 20:25:53 +0800

    first version

---
+++ include/handfree_hw/transport_serial.h
@@ -0,0 +1,77 @@
+/*
+ * transport_serial.h
+ *
+ *  Created on: Mar 29, 2016
+ *      Author: liao
+ */
+
+#ifndef TRANSPORT_SERIAL_H_
+#define TRANSPORT_SERIAL_H_
+
+#include <handfree_hw/transport.h>
+
+namespace handsfree_hw {
+
+class SerialParams {
+public:
+	std::string serialPort;
+	unsigned int baudRate;
+	unsigned int flowControl;
+	unsigned int parity;
+	unsigned int stopBits;
+	SerialParams() :
+			serialPort(), baudRate(921600), flowControl(0), parity(0), stopBits(0)
+	{
+	}
+	SerialParams(
+			std::string _serialPort,
+			unsigned int _baudRate,
+			unsigned int _flowControl,
+			unsigned int _parity,
+			unsigned int _stopBits
+			) :
+			serialPort(_serialPort),
+			baudRate(_baudRate),
+			flowControl(_flowControl),
+			parity(_parity),
+			stopBits(_stopBits)
+	{
+	}
+};
+
+class TransportSerial : public Transport {
+
+public:
+	TransportSerial ();
+	TransportSerial (std::string url);
+
+	virtual Buffer readBuffer();
+
+	virtual void writeBuffer(Buffer &data);
+
+private:
+	boost::shared_ptr<boost::asio::serial_port> port_;
+	SerialParams params_;
+	// for async read
+	Buffer temp_read_buf_;
+
+	boost::thread thread_;
+	// locks
+	boost::mutex port_mutex_;
+	boost::mutex write_mutex_;
+	boost::mutex read_mutex_;
+
+	bool initializeSerial();
+	void mainRun();
+
+	void start_a_read();
+	void start_a_write();
+	void readHandler(const boost::system::error_code &ec, size_t bytesTransferred);
+	void writeHandler(const boost::system::error_code &ec);
+};
+
+}
+
+
+
+#endif /* TRANSPORT_SERIAL_H_ */
